da8e56799ff06a379a6bc49bfa40df63565cc2eb
30052230 <30052230@B610-001>
2025-12-09 10:05:10 +0000

    Add project files.

---
+++ Taim_Website/Pages/Merch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Taim_Website.Pages
+{
+    public class MerchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
